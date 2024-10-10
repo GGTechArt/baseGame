fa5e2fef3ffc0d5be10278c549f3d2c8999da3a7
MiguelOrtegaLFTG <63127202+MiguelUGS@users.noreply.github.com>
2024-10-10 03:30:39 -0500

    Turrrets, Shop

--- Base/Assets/InstallSystem/Managers/SceneContextInstaller.cs
+++ Base/Assets/InstallSystem/Managers/SceneContextInstaller.cs
@@ -4,14 +4,8 @@ using UnityEngine;
 
 public class SceneContextInstaller : ServiceInstallerManager
 {
-    [SerializeField] protected ServiceInstallerBase[] installers;
     protected override void Awake()
     {
-        foreach (var installer in installers)
-        {
-            installer.GetComponent<IServiceInstaller>().InstallService();
-        }
-
         base.Awake();
     }
 }
--- Base/Assets/InstallSystem/Managers/ServiceInstallerManager.cs
+++ Base/Assets/InstallSystem/Managers/ServiceInstallerManager.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 public class ServiceInstallerManager : MonoBehaviour
 {
     [SerializeField] protected ServiceInstallerBase[] installersToInstantiate;
+    [SerializeField] protected ServiceInstallerBase[] installers;
     protected List<GameObject> installerGO = new List<GameObject>();
 
     protected virtual void Awake()
@@ -15,6 +16,11 @@ public class ServiceInstallerManager : MonoBehaviour
             installerInstance.transform.SetParent(transform);
             installerInstance.GetComponent<IServiceInstaller>().InstallService();
             installerGO.Add(installerInstance);
+        }
+
+        foreach (ServiceInstallerBase installer in installers)
+        {
+            installer.GetComponent<IServiceInstaller>().InstallService();
         }
 
         if (!FindFirstObjectByType<ProjectContextInstaller>())
