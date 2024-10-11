8208ead475c5d75dd6e1fb9753ec4aab306fb90f
GDaniel00 <Jdaniel.moreno00@gmail.com>
2024-10-11 00:57:49 -0500

    Actualizacion codigo menu principal

---
+++ Base/Assets/Scripts/MainMenuUIHandler.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using DG.Tweening;
+using Mono.Cecil;
+using Unity.VisualScripting;
+using UnityEditor.Rendering;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuUIHandler : MonoBehaviour
+{
+    [SerializeField] string nameSceneStart;
+    [SerializeField] float timeTransition;
+
+    [Space]
+    [SerializeField] GameObject optionsMenuPanel;
+
+    [Space]
+    [SerializeField] Button startButton;
+    [SerializeField] Button optionsButton;
+    [SerializeField] Button exitButton;
+
+    void Start()
+    {
+        startButton.onClick.AddListener(() => ServiceLocator.GetService<ScenesManager>().ChangeScene(nameSceneStart));
+        optionsButton.onClick.AddListener(() => ChangePanel(optionsMenuPanel));
+        exitButton.onClick.AddListener(() => Application.Quit());
+    }
+
+    public void ChangePanel(GameObject newPanel){
+        optionsMenuPanel.SetActive(true);
+        gameObject.SetActive(false);
+    }
+
+    public void Show()
+    {
+        gameObject.SetActive(true);
+        transform.DOScale(Vector3.one, timeTransition);
+    }
+
+    public void Hide()
+    {
+        transform.DOScale(Vector3.zero, timeTransition).OnComplete(() => {
+            gameObject.SetActive(false);
+        });
+    }
+}
