9b7aa730316d9eb03c0b0eae7dad0da77153c260
MiguelOrtegaLFTG <63127202+MiguelUGS@users.noreply.github.com>
2024-10-12 14:44:49 -0500

    Button sounds

--- Base/Assets/Scripts/LookAt.cs
+++ Base/Assets/Scripts/LookAt.cs
@@ -4,7 +4,12 @@ using UnityEngine;
 
 public class LookAt : MonoBehaviour
 {
+
     private void Start()
+    {
+        InvokeRepeating("LookCamera", 0, 0.1f);
+    }
+    public void LookCamera()
     {
         transform.forward = new Vector3(transform.position.x, Camera.main.transform.position.y, Camera.main.transform.position.z) - transform.position;
     }
